031f768ff2e4960ec132a41fbfbbe91a8079110f
byko <byko.dev@gmail.com>
2023-07-11 19:53:38 +0200

    data.php simple params validation

--- lib/data.php
+++ lib/data.php
@@ -4,8 +4,16 @@ require_once '../../../../init.php';
 
 use WHMCS\Module\Addon\Addon_WHMCS_Module\ClientsData;
 
+/* default values */
+$page = 1; $size = 3;
+
+if(isset($_GET["page"]) && isset($_GET["size"])){
+    $page = $_GET["page"];
+    $size = $_GET["size"];
+}
+
 $clientsData = new ClientsData();
-$records = $clientsData->getClientsData($_GET["page"], $_GET["size"]);
+$records = $clientsData->getClientsData($page, $size);
 
 header('Content-Type: application/json');
 echo  $clientsData->formatDataToJson($records);
